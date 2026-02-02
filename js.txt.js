4245ac94e6019637a808a7e21848582c81ca327a
kaikez <kaike.ribeiro@aluno.senai.br>
2026-02-02 15:37:17 -0300

    first commit

---
+++ js.txt.js
@@ -0,0 +1,12 @@
+let cor = "Azul";
+
+function pintar() {
+    let cor = "vermelho";
+return cor;
+    
+}
+
+pintar();
+console.log("fora da função",cor);
+
+
---
+++ script.js
@@ -0,0 +1,5 @@
+let contador = 0;
+function incrementar(){
+    contador += 1;
+    document.getElementById("contador").textContent = contador;
+}
